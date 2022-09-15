048c4d713daa074368eeca1ed64b4bc88d602982
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-15 14:25:31 +0200

    filter for dates during conert

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -10,12 +10,14 @@ df_cleaned <- df_cleaned[,c(1,2,4:7)]
 # Save cleaned data
 save(df_cleaned,file="./gen/data-preparation/output/data_cleaned.RData")
 
-
+###own data####
 #subset 
 Mexico <- subset(Mexico_listings, select = -c (host_name, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
 MexicoDuringConcert <- subset(Mexico$last_review, select = ("03-04-2022":"07-04-2022"))
 
-#filter dates during concert 
+#filter dates during concert  
 MexicoDuringConcert <- Mexico %>%
   filter(between(last_review, as.Date('2022-04-03'), as.Date('2022-04-07'))) %>%
-  arrange(last_review)
+  arrange(last_review) 
+
+
