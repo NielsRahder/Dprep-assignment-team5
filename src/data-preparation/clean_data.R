6693913608621fad7f6b8f5abd80796f73bb3045
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-27 13:55:04 +0200

    clean data

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -8,7 +8,6 @@ MexicoDuringConcert <- Mexico %>%
   filter(between(last_review, as.Date('2022-04-03'), as.Date('2022-04-07'))) %>%
   arrange(last_review) 
 
-
 #filter Dallas 
 Dallas <- subset(Dallas_Listings, select = -c (host_name, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
 MexicoDuringConcert <- subset(Mexico$last_review, select = ("06-04-2022":"06-04-2022"))
