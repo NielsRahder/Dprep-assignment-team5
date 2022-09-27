0b4c4d9f9c7f09240fa337a6135d14c9c0c6e928
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-27 13:21:45 +0200

    just a test form, needs additional data

---
+++ src/analysis/regression.R
@@ -0,0 +1,9 @@
+#shape of the regression 
+install.packages('fixest')
+library('fixest')
+
+reg1 <- feols(price ~ distance + 
+                   type + 
+                   distance:type, 
+                 data = Dallas_listings)
+tidy(reg1, conf.int = TRUE)
